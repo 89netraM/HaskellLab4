1ea3de9089f324ca2c8abc55aebbb3758b590d9c
Julia Jansson <julia.jansson@gmail.com>
2019-12-06 15:19:49 +0100

    loadData function

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
+import Data.IDX
+import Data.Matrix
+import Data.Vector
+import Data.Vector.Split
+import Data.Maybe
+
+imSize = 784
+
+--loadData:: FilePath -> FilePath -> IO (Maybe [(Int, Vector Int)])
+loadData file_lab file_dat = do
+    m_idx_lab <- decodeIDXLabelsFile file_lab
+    m_idx_dat <- decodeIDXFile file_dat
+    return (maybe Nothing (\idx_lab -> maybe Nothing (labeledIntData idx_lab) m_idx_dat) m_idx_lab)
+
 main = do
-    l <- getLine
-    putStrLn l
-    main
+    idxdat <- decodeIDXFile "train-images-idx3-ubyte"
+    let train_vector = chunksOf 784 (idxIntContent (fromJust idxdat))
+    return (Prelude.head train_vector)
