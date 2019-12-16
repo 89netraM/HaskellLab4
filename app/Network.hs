c2647cddc2292ab5b3205c36ac0d99f1b5148760
Julia Jansson <julia.jansson@gmail.com>
2019-12-16 13:57:50 +0100

    main loop done

--- app/Network.hs
+++ app/Network.hs
@@ -7,8 +7,8 @@ import Data.Random.Normal
 
 -------------------------------------
 -- | Types
-type Patterns = Matrix Int
-type Labels = Matrix Int
+type Pattern = Matrix Int
+type Label = Matrix Int
 
 type Weights = Matrix Double
 type Thresholds = Matrix Double
@@ -43,7 +43,7 @@ gradient = fmap (\x -> x*(1-x))
 
 -------------------------------------------------
 -- | The output error
-outputError:: Labels -> LayerResult -> Error
+outputError:: Label -> LayerResult -> Error
 outputError lab layRes = gradient layRes #* (fmap fromIntegral lab #- layRes)
 
 -- | err:    1*nOut
@@ -72,7 +72,34 @@ type Network = [Layer]
 network:: Int -> [Int] -> Network
 network seed (x:y:xs) = [Layer (initWeights n1 n2 r) (initThresholds n2) | (n1, n2, r) <- zip3 (x:y:xs) (y:xs) [0,1..]]
   where initWeights:: Int -> Int -> Int -> Weights
-        initWeights n1 n2 r = fromList n2 n1 (mkNormals' (0.0, 1.0 / (fromIntegral n1)) (seed + r))
+        initWeights n1 n2 r = fromList n2 n1 (mkNormals' (0.0, 1.0 / fromIntegral n1) (seed + r))
         initThresholds:: Int -> Thresholds
         initThresholds n2 = fromList 1 n2 [0.0,0.0..]
 network _ _ = error "Needs at least 2 layers: input and output"
+
+------------------------------------------------
+-- | Takes the network and input value (in double) and gives a list of layer results
+forwardLoop:: Network -> LayerResult -> [LayerResult]
+forwardLoop []     _     = []
+forwardLoop (l:ls) input = layRes : forwardLoop ls layRes
+  where layRes = forward l input
+
+-- | Takes the network and reversed layer result list and the output error and gives "reversed" error list
+backwardLoop:: Network -> [LayerResult] -> Error -> [Error]
+backwardLoop (Layer w th:ls) (lr:lrs) err = errOut : backwardLoop ls lrs errOut
+  where errOut = backward err w lr
+backwardLoop  _               _       _   = []
+
+-- | Takes a network, a list of errors, and a list of results and updates the network.
+updateLoop:: Network -> [Error] -> [LayerResult] -> Network
+updateLoop (l:ls) (e:es) (lr:lrs) = update l e lr : updateLoop ls es lrs
+updateLoop _      _      _        = []
+
+mainLoop:: Network -> [(Pattern, Label)] -> Network
+mainLoop net ((p, l) : pls) = mainLoop net2 pls
+  where layers = forwardLoop net (fmap fromIntegral p)
+        revLay = reverse layers
+        outErr = outputError l (head revLay)
+        errs   = reverse $ outErr : backwardLoop net (tail revLay) outErr
+        net2   = updateLoop net errs layers
+mainLoop net []             = net
