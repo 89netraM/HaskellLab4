56653d0dadd292f96782a374355121fdb0ecf01e
Mårten Åsberg <marten.asberg@outlook.com>
2019-12-16 16:03:08 +0100

    Testing files

--- app/Main.hs
+++ app/Main.hs
@@ -8,3 +8,9 @@ import Test.QuickCheck
 
 -- | Main function running the training, and testing
 main = undefined
+
+labDat = do
+    (Just raw) <- loadData train_labels train_images
+    return makeNiceData raw
+
+netw = network 45678 [784, 10]
