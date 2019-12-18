8698490df9718ac71a53c76e503d3ea0953471e5
Julia Jansson <julia.jansson@gmail.com>
2019-12-18 14:58:18 +0100

    Differentiate GUI fixed

--- app/Calculator.hs
+++ app/Calculator.hs
@@ -9,6 +9,7 @@ import Graphics.UI.Threepenny.Core as UI
 import qualified Graphics.UI.Threepenny as UI
 import Expr
 import Data.Maybe
+import Control.Monad
 
 canWidth,canHeight :: Num a => a
 canWidth  = 300
@@ -33,13 +34,16 @@ setup window =
      fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
      input   <- mkInput 20 "x"                -- The formula input
      draw    <- mkButton "Draw graph"         -- The draw button
-     zoom    <- mkInput 20 "0.004"            -- The zoom box
+     scale   <- mkHTML "Scale:"               -- The text "Scale:"
+     zoom    <- mkInput 20 "0.04"             -- The zoom box
+     diff    <- mkButton "Differentiate"      -- The differentiate button
        -- The markup "<i>...</i>" means that the text inside should be rendered
        -- in italics.
 
      -- Add the user interface elements to the page, creating a specific layout
-     formula <- row [pure fx,pure input]
-     getBody window #+ [column [pure canvas,pure formula,pure zoom,pure draw]]
+     formula <- row [pure fx, pure input, pure diff]
+     form2 <- row [pure scale, pure zoom] --Next row
+     getBody window #+ [column [pure canvas, pure formula, pure form2, pure draw]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
@@ -47,17 +51,29 @@ setup window =
      pure input # set style [("fontSize","14pt")]
 
      -- Interaction (install event handlers)
-     on UI.click     draw  $ \ _ -> readAndDraw input zoom canvas
-     on valueChange' input $ \ _ -> readAndDraw input zoom canvas
+     on UI.click     draw  $ \ _ -> readAndDraw input False zoom canvas
+     on UI.click     diff  $ \ _ -> do readAndDraw input True zoom canvas
+                                       dif input
+     on valueChange' input $ \ _ -> readAndDraw input False zoom canvas
 
+-- | Change the text in input field to be the derivative
+dif :: Element -> UI Element
+dif input = do
+   formula <- get value input
+   let exp = fromJust (readExpr formula)
+   let exp' = differentiate exp
+   pure input # set value (showExpr exp')
 
-readAndDraw :: Element -> Element -> Canvas -> UI ()
-readAndDraw input zoom canvas =
+readAndDraw ::Element -> Bool -> Element -> Canvas -> UI ()
+readAndDraw input diffBool zoom canvas =
   do -- Get the current formula (a String) from the input element
      formula <- get value input
      zoomStr <- get value zoom
      let exp = fromJust (readExpr formula)
      let ps = points exp (read zoomStr) (canWidth, canHeight)
+     let exp' = differentiate exp
+     --input # set value (showExpr exp')
+     let ps' = points exp' (read zoomStr) (canWidth, canHeight)
      -- Clear the canvas
      clearCanvas canvas
      -- The following code draws the formula text in the canvas and a blue line.
@@ -67,3 +83,4 @@ readAndDraw input zoom canvas =
      path "blue" [(canWidth/2,0),(canWidth/2,canHeight)] canvas
      path "blue" [(0,canHeight/2),(canWidth,canHeight/2)] canvas
      path "black" ps canvas
+     when diffBool $ path "black" ps' canvas
